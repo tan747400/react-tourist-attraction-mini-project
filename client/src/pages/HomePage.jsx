0e2cda3d6d9434659dfbb6cbb7281b11c3646998
Thanisorn <thanisorn.sir@gmail.com>
2025-09-06 07:38:58 +0700

    feat: add base HomePage with heading

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
-function HomePage() {
-  return (
-    <div>Homepage</div>
-  )
-}
+import { useState } from "react";
 
-export default HomePage
+export default function HomePage() {
+  const [keyword, setKeyword] = useState("");
+
+  return (
+    <div className="px-3 md:px-10 py-10">
+      <h1 className="text-5xl md:text-6xl text-center text-blue-400 font-bold mb-10">
+        เที่ยวไหนดี
+      </h1>
+    </div>
+  );
+}
