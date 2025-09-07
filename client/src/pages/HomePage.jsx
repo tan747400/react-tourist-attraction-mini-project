7f69dc5f0d15d5071b302e2a1876f3e7e5408132
Thanisorn <thanisorn.sir@gmail.com>
2025-09-07 09:22:17 +0700

    feat: integrate Axios for API calls and implement trip fetching in HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,33 @@
 import { useEffect, useRef, useState } from "react";
 import SearchBar from "../components/SearchBar";;
+import { getTrips } from "../services/tripService";
+
 
 export default function HomePage() {
   const [keyword, setKeyword] = useState("");
   const inputRef = useRef(null);
+  const [trips, setTrips] = useState([]);
+  const [status, setStatus] = useState("idle");
+
+  const fetchData = async (kw = "") => {
+    try {
+      setStatus("loading");
+      const data = await getTrips(kw);
+      setTrips(data);
+      setStatus("success");
+    } catch {
+      setStatus("error");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useDebouncedEffect(() => {
+    fetchData(keyword);
+    inputRef.current?.focus();
+  }, [keyword], 300);
 
   return (
     <div className="px-3 md:px-10 py-10">
